5c774d5629f113fe378e8a10b8aba2b5aa72e718
James Jory <james@jamesjory.com>
2025-08-01 16:04:20 -0700

    Popularity count demo article

--- src/web-ui/src/partials/AppModal/DemoGuide/DemoGuideArticle/articles/PopularityCount.vue
+++ src/web-ui/src/partials/AppModal/DemoGuide/DemoGuideArticle/articles/PopularityCount.vue
@@ -13,23 +13,24 @@
     </template>
 
     <p>
-      Recommends popular products based on how many times customers have viewed an item. The most popular items have the most
-      "View" interactions with unique users. The recipe returns the same popular items for all users. Therefore, recommendations are
+      Recommends popular products based on how many times customers have interacted with items. For this demo, we want a popularity model 
+      that recommends the most popular products based on clickstream events. Therefore, the model is trained with the Popularity-Count recipe 
+      only on "View" event types. The recipe returns the same popular items for all users. Therefore, recommendations are
       not personalized to the individual user and are useful when you want a system-wide representation of what's popular.
     </p>
 
     <p>
-      Popular by Views is implemented used on the homepage to display popular items when there is not browsing
+      Popularity Count is used on the homepage to display popular items when there is no browsing
       history for the current user (i.e. cold user). Once the known or anonymous user has more than three interactions (this
       is an arbitrary number chosen in the Retail Demo Store's implementation), the storefront will switch to a personalized
-      recommendation using the Recommended For You retail recommender. You can tell when this transition is made when the homepage
+      recommendation using the User Personalization model. You can tell when this transition is made when the homepage
       grid header changes from "Popular products" to "Inspired by your shopping trends".
     </p>
 
     <p>
       You may notice some items are annotated with a "Promoted" banner across the top of the product image. This banner indicates
       products that match a promotional filter that is optionally applied when retrieving recommendations. Promotional filters are
-      a great way to ensure that a user-defined percentage of recommended items match a specific filter expression but are also
+      a great way to ensure that a customer-defined percentage of recommended items match a specific filter expression but are also
       relevant to the user. This can be used to highlight products on sale or new products recently added to the catalog.
     </p>
 
@@ -43,7 +44,7 @@
 import ArticleLayout from '../ArticleLayout.vue';
 
 export default {
-  name: 'PopularByViews',
+  name: 'PopularityCount',
   components: { ArticleLayout },
 };
 </script>
